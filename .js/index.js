cf4e93b8c51db8f7f60f931f0de31610ff5f1c5d
vikas singh <pokhriyalvikas940@gmail.com>
2024-03-08 15:00:34 +0530

    new project

---
+++ .js/index.js
@@ -0,0 +1,15 @@
+let cart_icon = document.getElementById('cart');
+let cart_container = document.getElementById('cart-container');
+
+
+cart_icon.addEventListener('click', () => {
+    cart_container.classList.toggle('cart-show');
+})
+
+
+
+
+
+
+
+
